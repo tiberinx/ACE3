42c2ec85ee4ce373a96eb0c3365e9aa897e3a9eb
Thomas Kooi <thomasjkooi@gmail.com>
2018-05-21 21:04:39 +0200

    Add armake builds to cricleci as experimental build step

--- addons/vehiclelock/CfgVehicles.hpp
+++ addons/vehiclelock/CfgVehicles.hpp
@@ -5,14 +5,14 @@
             condition = QUOTE(([ARR_2(_player, _target)] call FUNC(hasKeyForVehicle)) && {(locked _target) in [ARR_2(2,3)]}); \
             statement = QUOTE([ARR_3(QUOTE(QGVAR(setVehicleLock)), [ARR_2(_target,false)], [_target])] call CBA_fnc_targetEvent); \
             priority = 0.3; \
-            icon = QPATHTOF(UI\key_menuIcon_ca.paa); \
+            icon = QPATHTOF(ui\key_menuIcon_ca.paa); \
         }; \
         class ACE_lockVehicle { \
             displayName = CSTRING(Action_Lock); \
             condition = QUOTE(([ARR_2(_player, _target)] call FUNC(hasKeyForVehicle)) && {(locked _target) in [ARR_2(0,1)]}); \
             statement = QUOTE([ARR_3(QUOTE(QGVAR(setVehicleLock)), [ARR_2(_target,true)], [_target])] call CBA_fnc_targetEvent); \
             priority = 0.2; \
-            icon = QPATHTOF(UI\key_menuIcon_ca.paa); \
+            icon = QPATHTOF(ui\key_menuIcon_ca.paa); \
         }; \
         class ACE_lockpickVehicle { \
             displayName = CSTRING(Action_Lockpick); \
@@ -30,7 +30,7 @@
                 statement = QUOTE([ARR_3(QUOTE(QGVAR(setVehicleLock)), [ARR_2(_target,false)], [_target])] call CBA_fnc_targetEvent); \
                 exceptions[] = {"isNotSwimming"}; \
                 priority = 0.3; \
-                icon = QPATHTOF(UI\key_menuIcon_ca.paa); \
+                icon = QPATHTOF(ui\key_menuIcon_ca.paa); \
             }; \
             class ACE_lockVehicle { \
                 displayName = CSTRING(Action_Lock); \
@@ -39,7 +39,7 @@
                 statement = QUOTE([ARR_3(QUOTE(QGVAR(setVehicleLock)), [ARR_2(_target,true)], [_target])] call CBA_fnc_targetEvent); \
                 exceptions[] = {"isNotSwimming"}; \
                 priority = 0.2; \
-                icon = QPATHTOF(UI\key_menuIcon_ca.paa); \
+                icon = QPATHTOF(ui\key_menuIcon_ca.paa); \
             }; \
             class ACE_lockpickVehicle { \
                 displayName = CSTRING(Action_Lockpick); \
@@ -85,7 +85,7 @@ class CfgVehicles {
         scope = 1;
         isGlobal = 1;
         isSingular = 1;
-        icon = QPATHTOF(UI\Icon_Module_VehicleLock_ca.paa);
+        icon = QPATHTOF(ui\Icon_Module_VehicleLock_ca.paa);
         functionPriority = 0;
         class Arguments {
             class LockVehicleInventory {
@@ -123,7 +123,7 @@ class CfgVehicles {
         function = QFUNC(moduleSync);
         scope = 2;
         isGlobal = 0;
-        icon = QPATHTOF(UI\Icon_Module_VehicleKey_ca.paa);
+        icon = QPATHTOF(ui\Icon_Module_VehicleKey_ca.paa);
         functionPriority = 0;
         class Arguments {};
         class ModuleDescription: ModuleDescription {
